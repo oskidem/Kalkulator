433240c22f34cae792932cb6e2275883b7899918
DominikaGit <dominika.pawl@gmail.com>
2022-12-17 22:20:41 +0100

    Poprawienie bledu w tresci printf

--- main.c
+++ main.c
@@ -3,41 +3,49 @@
 #include "odejmowanie.h"
 #include "mnozenie.h"
 #include "dzielenie.h"
+#include "modulo.h"
 
 int main() {
     int a,b,wybor;
-    printf("Podaj jakie dzialanie chcesz wykonac: \n1-dodawanie\n2-odejmowanie\n3-dzielenie\n4-mnozenie\n");
+    printf("Podaj jakie dzialanie chcesz wykonac: \n1-dodawanie\n2-odejmowanie\n3-dzielenie\n4-mnozenie\n5-modulo\n");
     scanf("%d",&wybor);
 
     switch (wybor){
         case (1):
             printf("Podaj pierwsza liczbe: ");
             scanf("%d",&a);
-            printf("Podaj pierwsza liczbe: ");
+            printf("Podaj druga liczbe: ");
             scanf("%d",&b);
             printf("Wynik: %d", dodawanie(a,b));
             break;
         case (2):
             printf("Podaj pierwsza liczbe: ");
             scanf("%d",&a);
-            printf("Podaj pierwsza liczbe: ");
+            printf("Podaj druga liczbe: ");
             scanf("%d",&b);
             printf("Wynik: %d", odejmowanie(a, b));
             break;
         case (3):
             printf("Podaj pierwsza liczbe: ");
             scanf("%d",&a);
-            printf("Podaj pierwsza liczbe: ");
+            printf("Podaj druga liczbe: ");
             scanf("%d",&b);
             printf("Wynik: %d", dzielenie(a, b));
             break;
         case (4):
             printf("Podaj pierwsza liczbe: ");
             scanf("%d",&a);
-            printf("Podaj pierwsza liczbe: ");
+            printf("Podaj druga liczbe: ");
             scanf("%d",&b);
             if (b == 0) printf ("Nie dzile przez 0!");
             else printf("Wynik: %d", mnozenie(a, b));
+            break;
+        case (5):
+            printf("Podaj pierwsza liczbe: ");
+            scanf("%d",&a);
+            printf("Podaj druga liczbe: ");
+            scanf("%d",&b);
+            printf("Wynik: %d", modulo(a, b));
             break;
     }
     return 0;
